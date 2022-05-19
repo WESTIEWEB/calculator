42191905ada56ad677bd88264c8040aeca7b7999
chibucs2012 <ilocibuike@gmail.com>
2022-05-19 13:08:11 +0100

    first version of calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+// a function that performs mathematical operations.
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function mult(a, b) {
+    return a * b;
+}
